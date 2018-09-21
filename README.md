e5963b724ade8b484d4ca1df96b99b27d3854a37
ApoorvaJu08 <apoorva10339@gmail.com>
2018-09-21 20:47:43 +0530

    slider without text done

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Pre-requirements
+Node js and npm
+
 # Home
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.5.
@@ -5,6 +8,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+command  : ng serve --open
 
 ## Code scaffolding
 
--- src/app/speakers/speakers.component.ts
+++ src/app/speakers/speakers.component.ts
@@ -1,51 +1,13 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-speakers',
   templateUrl: './speakers.component.html',
-  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./speakers.component.css']
 })
 export class SpeakersComponent implements OnInit {
-  closeResult: string;
 
-  constructor(private modalService: NgbModal) {}
-
-  open(content) {
-    this.modalService.open(content, {windowClass: 'dark-modal', size: 'lg', ariaLabelledBy: 'modal-basic-title', centered: true }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  open1(content1) {
-    this.modalService.open(content1, {windowClass: 'dark-modal', size: 'lg', ariaLabelledBy: 'modal-basic-title', centered: true }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  open2(content2) {
-    this.modalService.open(content2, {windowClass: 'dark-modal', size: 'lg', ariaLabelledBy: 'modal-basic-title', centered: true }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
-  }
-
+  constructor() {}
 
   ngOnInit() {
   }
